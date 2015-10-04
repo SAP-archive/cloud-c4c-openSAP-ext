86a69dbe4e995f6bbd3bfbd94d8559fcf7cf3a5c
Krasimir Semerdzhiev <evilyeti@gmail.com>
2015-10-05 00:52:15 +0200

    updated HTML5 app to accomodate the opportunity risk assessment

--- MyC4COpportunity/view/Opportunity.controller.js
+++ MyC4COpportunity/view/Opportunity.controller.js
@@ -1,41 +1,34 @@
 sap.ui.controller("view.Opportunity", {
-	
-	oDataServiceURL: "c4codata",
+
+	oDataC4CServiceURL: "/destinations/c4codata",
+	oDataHCPServiceUrl: "/destinations/c4cext.svc/Opportunities/Risk",
 
 	/**
 	 * Called when a controller is instantiated and its View controls (if available) are already created.
 	 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 	 * @memberOf view.Opportunity
 	 */
-		onInit: function() {
-			this.oDataModel = new sap.ui.model.odata.ODataModel( this.oDataServiceURL );
-			this.getView().setModel(this.oDataModel);
+	onInit: function() {
+		var oDataModel = new sap.ui.model.odata.ODataModel(this.oDataC4CServiceURL);
+		oDataModel.attachRequestCompleted({}, function(){
+			var oList = this.getView().byId("opportunitiesList");
+			oList.setSelectedIndex(0);
+		}, this);
+		this.getView().setModel(oDataModel);
+
+		this.getView().setModel(new sap.ui.model.json.JSONModel(), "riskModel");
+	},
+
+	onListItemPress: function(evt) {
+
+		var ctx = evt.getParameter("rowContext");
+		if (ctx){
+			var oForm = this.getView().byId("opportunityDetails");
+			oForm.bindElement(ctx.getPath());
+
+			//fetch the Risk value using the RiskAssessment application
+			var riskModel = this.getView().getModel("riskModel");
+			riskModel.loadData(this.oDataHCPServiceUrl + "?opportunityID=" + ctx.getObject().OpportunityID);
 		}
-
-	/**
-	 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-	 * (NOT before the first rendering! onInit() is used for that one!).
-	 * @memberOf view.Opportunity
-	 */
-	//	onBeforeRendering: function() {
-	//
-	//	},
-
-	/**
-	 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-	 * This hook is the same one that SAPUI5 controls get after being rendered.
-	 * @memberOf view.Opportunity
-	 */
-	//	onAfterRendering: function() {
-	//
-	//	},
-
-	/**
-	 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-	 * @memberOf view.Opportunity
-	 */
-	//	onExit: function() {
-	//
-	//	}
-
+	}
 });
